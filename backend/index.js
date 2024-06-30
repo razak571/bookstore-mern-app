488056e6f5f62809d936af00d531bd16bb11fcc8
Razak Attar <165865038+razak571@users.noreply.github.com>
2024-07-01 01:30:14 +0530

    upadet

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const mongoDBURL = process.env.mongoDBURL;
+const mongoDBURL = process.env.MONGODBURL;
 const baseUrl = process.env.BASEURL;
 
 app.use(express.json());
